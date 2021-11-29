58b184fb018c0326d04c9fd4c44be4416ba8a8dc
roey <roey132@gmail.com>
2021-11-29 13:50:29 +0200

    fixed timer not loading on first second

--- script.js
+++ script.js
@@ -6,35 +6,43 @@ window.addEventListener('resize',resizeCanvas,false)
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    canvas.fillStyle = '#3464eb'
 }
 
 resizeCanvas()
 
-const jsConfetti = new JSConfetti({ canvas });
+const jsConfetti = new JSConfetti({ canvas })
+
 jsConfetti.addConfetti({
   emojis: ['עד מתי' ,' כמה עוד'],
-  confettiRadius: 20,
-  confettiNumber: 50,
-  emojiSize: 100,
-});
+  confettiRadius: 10,
+  confettiNumber: 35,
+  emojiSize: 70,
+})
 
 var countDownDate = new Date('Jun 30, 2022 00:00:00').getTime()
 
-var myfunc = setInterval(function () {
-    var now = new Date().getTime()
-    var timeleft = countDownDate - now
+function createTimer() {
 
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+  var now = new Date().getTime()
+  var timeleft = countDownDate - now
 
-    const fixeddays = ('' + days).padStart(3, '0')
-    const fixedhours = ('' + hours).padStart(2, '0')
-    const fixedminutes = ('' + minutes).padStart(2, '0')
-    const fixedseconds = ('' + seconds).padStart(2, '0')
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-    var counter = `${fixeddays}:${fixedhours}:${fixedminutes}:${fixedseconds}`
+  const fixeddays = ('' + days).padStart(3, '0')
+  const fixedhours = ('' + hours).padStart(2, '0')
+  const fixedminutes = ('' + minutes).padStart(2, '0')
+  const fixedseconds = ('' + seconds).padStart(2, '0')
 
-    document.getElementById('timer').innerHTML = counter
+  var counter = `${fixeddays}:${fixedhours}:${fixedminutes}:${fixedseconds}`
+
+  document.getElementById('timer').innerHTML = counter
+}
+
+createTimer()
+var myfunc = setInterval(function(){
+  createTimer()
 }, 1000)
